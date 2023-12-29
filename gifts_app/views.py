8e7b68ca3cf8406fcce06768ed2aa71e173096e8
unknown <sean.herbison@gmail.com>
2023-12-29 14:57:09 -0700

    Starts views, lays out methods for updating database.

--- gifts_app/views.py
+++ gifts_app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import GiftSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+@api_view(["POST"])
+def create_gift(request):
+    pass
+
+@api_view(["GET"])
+def get_self_view(request):
+    pass
+
+@api_view(["GET"])
+def get_other_view(request):
+    pass
+
+@api_view(["PUT"])
+def update_gift(request):
+    pass
+
+@api_view(["DELETES"])
+def remove_gift(request):
+    pass
