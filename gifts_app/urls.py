bbfb7e5fd09ad93af680043cb8cf1b393b11c856
unknown <sean.herbison@gmail.com>
2024-01-02 16:22:30 -0700

    Includes app URLs in project URLs. Builds create_gift_self and adds create_gift_other to views. Updates app URLs for new views.

--- gifts_app/urls.py
+++ gifts_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import create_gift, get_self_view, get_other_view, update_gift, remove_gift
+from .views import create_gift_self, create_gift_other, get_self_view, get_other_view, update_gift, remove_gift
 
 
 urlpatterns = [
-    path('create/', create_gift, name='create_gift'),
+    path('create_gift_self/', create_gift_self, name='create_gift_self'),
+    path('create_gift_other/', create_gift_other, name='create_gift_other'),
 ]
--- gifts_app/views.py
+++ gifts_app/views.py
@@ -4,9 +4,27 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import GiftSerializer
 from django.contrib.auth.models import User
+from .forms import AddOtherForm, AddSelfForm
+from django.http import JsonResponse
+
+
 
 @api_view(["POST"])
-def create_gift(request):
+def create_gift_self(request):
+    form = AddOtherForm(request.POST)
+    if form.is_valid():
+        gift_id = form.cleaned_data.get("gift_id")
+        #Add other fields
+    return JsonResponse(
+        {
+            "message": "Gift added successfully",
+        }
+    )
+    pass
+
+@api_view(["POST"])
+def create_gift_other(request):
+    form = AddSelfForm(request.POST)
     pass
 
 @api_view(["GET"])
