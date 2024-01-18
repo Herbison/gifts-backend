e7335b2449fa17e8993b1af5dfb50991801314a8
unknown <sean.herbison@gmail.com>
2024-01-18 10:27:16 -0700

    Adds url to get members.

--- gifts_app/urls.py
+++ gifts_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import get_all_gifts
+from .views import get_all_gifts, get_all_members
 
 
 urlpatterns = [
     path('get_all_gifts/', get_all_gifts, name='get_all_gifts'),
+    path('<get_all_members/', get_all_members, name='meet_results'),
 ]
