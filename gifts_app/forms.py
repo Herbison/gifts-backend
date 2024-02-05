11cf186e52173b182806775296cb9edb81121811
unknown <sean.herbison@gmail.com>
2024-02-05 09:34:50 -0700

    Adds widget to forms to show visibility as checkboxes instead of a multi-select box.

--- gifts_app/forms.py
+++ gifts_app/forms.py
@@ -5,4 +5,7 @@ class GiftForm(forms.ModelForm):
     class Meta:
         model = Gift
         fields = ['gift_receiver', 'item_name', 'exact_item', 'multiple', 'notes', 'visible_to', 'links']
+        widgets = {
+            'visible_to': forms.CheckboxSelectMultiple(),
+        }
         # Only fields I want to show. How do I handle visibility, which is only visible to selfMember?
