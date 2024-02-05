9b00eda92874da10930751a0449778b0fb7ee25a
unknown <sean.herbison@gmail.com>
2024-02-04 21:18:49 -0700

    Adds comments, cleans up extra space.

--- gifts_app/forms.py
+++ gifts_app/forms.py
@@ -4,4 +4,5 @@ from .models import Gift
 class GiftForm(forms.ModelForm):
     class Meta:
         model = Gift
-        fields = ['gift_receiver', 'item_name', 'exact_item', 'multiple', 'notes', 'date_to_remove', 'visible_to', 'links']
+        fields = ['gift_receiver', 'item_name', 'exact_item', 'multiple', 'notes', 'visible_to', 'links']
+        # Only fields I want to show. How do I handle visibility, which is only visible to selfMember?
