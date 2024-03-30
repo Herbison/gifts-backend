8410cda00254978c4951d9155b770d527f658f34
unknown <sean.herbison@gmail.com>
2024-03-29 17:10:43 -0700

    Adjusts form fields

--- gifts_app/forms.py
+++ gifts_app/forms.py
@@ -4,7 +4,7 @@ from .models import Gift
 class GiftForm(forms.ModelForm):
     class Meta:
         model = Gift
-        fields = ['gift_receiver', 'item_name', 'exact_item', 'multiple', 'notes', 'visible_to', 'links']
+        fields = ['gift_adder', 'gift_receiver', 'item_name', 'exact_item', 'multiple', 'notes', 'visible_to']
         widgets = {
             'visible_to': forms.CheckboxSelectMultiple(),
         }
