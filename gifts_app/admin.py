9e27c57666c34643caae0ff2a5ff70ef906bbbb2
unknown <sean.herbison@gmail.com>
2024-04-26 10:39:30 -0700

    Registers GiftAdmin and Link with admin.py

--- gifts_app/admin.py
+++ gifts_app/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from .models import Member, Gift
+from .models import Member, Gift, Link
 
+class LinkInline(admin.TabularInline):
+    model = Link
+    extra = 1
+
+class GiftAdmin(admin.ModelAdmin):
+    inlines = [LinkInline]
+
+admin.site.register(Gift, GiftAdmin)
 admin.site.register(Member)
-admin.site.register(Gift)
+admin.site.register(Link)
